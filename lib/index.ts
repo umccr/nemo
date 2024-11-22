e869ebcc7da2c95c723002fe6d8346de0817db0a
pdiakumis <peterdiakumis@gmail.com>
2024-11-22 16:07:02 +1100

    spin up Fargate Service

--- lib/index.ts
+++ lib/index.ts
@@ -1,14 +1,23 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { ContainerImage } from 'aws-cdk-lib/aws-ecs';
+import { ApplicationLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns';
 import { Construct } from 'constructs';
-import { S3Bucket } from './constructs/S3Bucket';
+// import { S3Bucket } from './constructs/S3Bucket';
 
 export class NemoWebStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const bucket = new S3Bucket(this, 'MyRemovableBucket', {
-      environment: 'dev',
+    new ApplicationLoadBalancedFargateService(this, 'MyWebServer', {
+      taskImageOptions: {
+        image: ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+      },
+      publicLoadBalancer: true,
     });
+
+    // const bucket = new S3Bucket(this, 'MyRemovableBucket', {
+    //   environment: 'dev',
+    // });
 
     // add a lambda function that works with docker image
     // const lambda = new DockerImageFunction(this, 'MyFunction', {
