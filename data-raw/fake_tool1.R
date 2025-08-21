3597ac173003805fa601e8899e7dd7e5204269ec
pdiakumis <peterdiakumis@gmail.com>
2025-08-21 12:15:11 +1000

    add fake tool1 data

---
+++ data-raw/fake_tool1.R
@@ -0,0 +1,46 @@
+tool1 <- list(
+  table1 = list(
+    list(
+      version = "v1.2.3",
+      data = tibble::tibble(
+        SampleID = "sampleA",
+        Chromosome = c("chr1", "chr2", "chr3"),
+        Start = c(10, 100, 1000),
+        End = c(50, 500, 5000),
+        metricX = c(0.1, 0.2, 0.3)
+      )
+    ),
+    list(
+      version = "latest",
+      data = tibble::tibble(
+        SampleID = "sampleA",
+        Chromosome = c("chr1", "chr2", "chr3"),
+        Start = c(10, 100, 1000),
+        End = c(50, 500, 5000),
+        metricX = c(0.1, 0.2, 0.3),
+        metricY = c(0.4, 0.5, 0.6),
+        metricZ = c(0.7, 0.8, 0.9)
+      )
+    )
+  ),
+  table2 = list(
+    list(
+      version = "latest",
+      data = tibble::tibble(
+        SampleID = "sampleA",
+        metricX = c("a", "b", "c"),
+        metricY = c(12.3, 4.56, 7.89)
+      )
+    )
+  )
+)
+
+purrr::map2(tool1, names(tool1), \(tab, tab_name) {
+  stopifnot(length(tab_name) == 1)
+  purrr::map(tab, \(entry) {
+    odir <- here::here("inst/extdata", "tool1", entry$version) |>
+      fs::dir_create()
+    fname <- file.path(odir, glue::glue("tool1_{tab_name}.tsv"))
+    readr::write_tsv(entry$data, fname)
+  })
+})
